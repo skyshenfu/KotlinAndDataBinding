b0fa17b56cdc37bb7d90e80359fbf8268791f770
painworkpc <zhangty@all-help.com>
2017-05-27 12:56:52 +0800

    加入了漏掉提交的文件

---
+++ app/src/main/java/com/pain/wetestkotlin/utils/RxBus.java
@@ -0,0 +1,39 @@
+package com.pain.wetestkotlin.utils;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
+
+public class RxBus {
+
+    private final Subject<Object> mBus;
+
+    private RxBus() {
+        // toSerialized method made bus thread safe
+        mBus = PublishSubject.create().toSerialized();
+    }
+
+    public static RxBus getInstance() {
+        return Holder.BUS;
+    }
+
+    public void post(Object obj) {
+        mBus.onNext(obj);
+    }
+
+    public <T> Observable<T> toObservable(Class<T> tClass) {
+        return mBus.ofType(tClass);
+    }
+
+    public Observable<Object> toObservable() {
+        return mBus;
+    }
+
+    public boolean hasObservers() {
+        return mBus.hasObservers();
+    }
+
+    private static class Holder {
+        private static final RxBus BUS = new RxBus();
+    }
+}
